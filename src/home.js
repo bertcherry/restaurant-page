71efad30259ece403ec73fc314b3e1434ac89456
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-04-05 13:40:37 -0700

    add home content and onload

---
+++ src/home.js
@@ -0,0 +1,54 @@
+import logo from './logo.jpg';
+
+const home = () => {
+    const content = document.getElementById('content');
+    
+    const header = document.createElement('div');
+
+    const name = document.createElement('h1');
+    name.textContent = 'Bert\'s Beignets by Bike';
+    header.appendChild(name);
+
+    const logoContainer = document.createElement('div');
+    const logoImage = document.createElement('img');
+    logoImage.src = logo;
+    logoContainer.appendChild(logoImage);
+    header.appendChild(logoContainer);
+
+    content.appendChild(header);
+
+    const quote = document.createElement('div');
+    
+    const quoteText = document.createElement('div');
+    quoteText.textContent = 'These beignets are incredible! And having them delivered to me by a carbon-neutral service? Even better. The coffee is hot, the jam is gooey, and the vibes are great!';
+    quote.appendChild(quoteText);
+
+    const quoteAttribution = document.createElement('div');
+    quoteAttribution.textContent = 'Seph Tacular';
+    quote.appendChild(quoteAttribution);
+
+    content.appendChild(quote);
+
+    const hours = document.createElement('div');
+    
+    const hoursTitle = document.createElement('h2');
+    hoursTitle.textContent = 'Hours';
+    hours.appendChild(hoursTitle);
+
+    const hoursData = document.createElement('ul');
+    const hoursOne = document.createElement('li');
+    const hoursTwo = document.createElement('li');
+    hoursOne.textContent = 'Saturday 9am - 12pm';
+    hoursTwo.textContent = 'Sunday 9am - 2pm';
+    hoursData.appendChild(hoursOne);
+    hoursData.appendChild(hoursTwo);
+    hours.appendChild(hoursData);
+
+    const hoursText = document.createElement('div');
+    hoursText.textContent = 'Please contact us to arrange your order and delivery.';
+    hours.appendChild(hoursText);
+
+    content.appendChild(hours);
+};
+
+export default home;
--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import './style.css';
 import home from './home';
 import menu from './menu';
 import contact from './contact';
+
+window.onload = home();
