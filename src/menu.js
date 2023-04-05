308b5649ca93afa7a4c2678d9389f2047618ef73
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-04-05 14:59:33 -0700

    docs id to lower case

--- src/menu.js
+++ src/menu.js
@@ -4,7 +4,7 @@ const menuSection = (title) => {
     divTitle.textContent = title;
     div.appendChild(divTitle);
     const items = document.createElement('ul');
-    items.id = title;
+    items.id = title.toLowerCase();
     div.appendChild(items);
     container.appendChild(div);
 }
@@ -30,18 +30,18 @@ const menuItem = (group, name, price, description) => {
 
 const menu = () => {
     const beignets = menuSection('Beignets');
-    const plain = menuItem('Beignets', 'Plain', '$2/ea', 'Unsugared beignets ready to add jam or leave savory.')
-    const powdered = menuItem('Beignets', 'Powdered', '$3/ea', 'Dusted with powdered sugar for classic style.')
-    const cinnamon = menuItem('Beignets', 'Plain', '$2/ea', 'Rolled in cinnamon sugar to spice up your life.')
+    const plain = menuItem('beignets', 'Plain', '$2/ea', 'Unsugared beignets ready to add jam or leave savory.')
+    const powdered = menuItem('beignets', 'Powdered', '$3/ea', 'Dusted with powdered sugar for classic style.')
+    const cinnamon = menuItem('beignets', 'Plain', '$2/ea', 'Rolled in cinnamon sugar to spice up your life.')
 
     const sides = menuSection('Sides');
-    const berry = menuItem('Sides', 'Berry Jam', '$3', 'A container of homemade seasonal berry jam.')
-    const marmalade = menuItem('Sides', 'Marmalade', '$3', 'Imported spiced orange marmalade.')
+    const berry = menuItem('sides', 'Berry Jam', '$3', 'A container of homemade seasonal berry jam.')
+    const marmalade = menuItem('sides', 'Marmalade', '$3', 'Imported spiced orange marmalade.')
 
     const beverages = menuSection('Beverages');
-    const coffee = menuItem('Beverages', 'Drip Coffee', '$15', 'A warm carafe of coffee, light roast or nothing.')
-    const juice = menuItem('Beverages', 'Orange Juice', '$8', '16oz of hand squeezed orange juice.')
-    const mimosa = menuItem('Beverages', 'Mimosa', '$12', 'A flute of champagne with 4 drops of fresh orange juice.')
+    const coffee = menuItem('beverages', 'Drip Coffee', '$15', 'A warm carafe of coffee, light roast or nothing.')
+    const juice = menuItem('beverages', 'Orange Juice', '$8', '16oz of hand squeezed orange juice.')
+    const mimosa = menuItem('beverages', 'Mimosa', '$12', 'A flute of champagne with 4 drops of fresh orange juice.')
 }
 
 export default menu;
