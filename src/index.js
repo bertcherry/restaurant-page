b4b1c30b7670593bd06c20e02e1bcdae7b590332
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-04-05 14:18:17 -0700

    add tabulation listeners

--- src/index.js
+++ src/index.js
@@ -7,14 +7,40 @@ window.onload = () => {
     const content = document.getElementById('content');
     const nav = document.createElement('div');
     const navHome = document.createElement('button');
+    navHome.addEventListener('click', () => {
+            clearContainer();
+            home();
+        }
+    );
     navHome.textContent = 'Home';
     const navMenu = document.createElement('button');
+    navMenu.addEventListener('click', () => {
+            clearContainer();
+            menu();
+        }
+    );
     navMenu.textContent = 'Menu';
     const navContact = document.createElement('button');
+    navContact.addEventListener('click', () => {
+            clearContainer();
+            contact();
+        }
+    );
     navContact.textContent = 'Contact';
+
     nav.appendChild(navHome);
     nav.appendChild(navMenu);
     nav.appendChild(navContact);
     content.appendChild(nav);
+    
     home();
 };
+
+const clearContainer = () => {
+    const container = document.getElementById('container');
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+}
+
+const navMenu = document.getElementById('menu');
