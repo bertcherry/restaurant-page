2847f895afc17d278fd57fff9a91053ae7691a68
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-04-05 17:52:22 -0700

    style contact form

--- src/contact.js
+++ src/contact.js
@@ -7,11 +7,20 @@ const formField = (type, label) => {
     fieldLabel.textContent = label;
     fieldWrapper.appendChild(fieldLabel);
 
-    const field = document.createElement('input');
-    field.setAttribute('type', type);
-    field.setAttribute('name', label.toLowerCase());
-    field.id = label.toLowerCase();
-    fieldWrapper.appendChild(field);
+    if (type == 'textarea') {
+        const field = document.createElement('textarea');
+        field.setAttribute('cols', '21');
+        field.setAttribute('rows', '10');
+        field.setAttribute('name', label.toLowerCase());
+        field.id = label.toLowerCase();
+        fieldWrapper.appendChild(field);
+    } else {
+        const field = document.createElement('input');
+        field.setAttribute('type', type);
+        field.setAttribute('name', label.toLowerCase());
+        field.id = label.toLowerCase();
+        fieldWrapper.appendChild(field);
+    }
 
     const form = document.getElementById('contact-form');
     form.appendChild(fieldWrapper);
@@ -26,6 +35,7 @@ const contact = () => {
     info.textContent = 'We deliver on weekend mornings by specific arrangement throughout Seattle\'s Central District, Capitol Hill, and First Hill. Contact us to set up your order!';
     
     const formContainer = document.createElement('div');
+    formContainer.classList.add('form-container');
     const form = document.createElement('form');
     form.id = 'contact-form';
     formContainer.appendChild(form);
@@ -35,7 +45,7 @@ const contact = () => {
 
     const name = formField('text', 'Name');
     const email = formField('email', 'Email');
-    const message = formField('text', 'Message');
+    const message = formField('textarea', 'Message');
     
     const submit = document.createElement('button');
     submit.textContent = 'Send';
